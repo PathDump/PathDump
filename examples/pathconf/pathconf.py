4070765bef5ccd97cbcdf6816392fcc8ddf28263
Praveen Tammana <praveen.thammana@gmail.com>
2016-10-04 01:58:10 +0100

    simplified design path conformance of application

--- examples/pathconf/pathconf.py
+++ examples/pathconf/pathconf.py
@@ -5,10 +5,8 @@ import sys
 #raises alarm to controller.
 if __name__ == "__main__":
     tree = capi.getAggTree (['controller'])
-    subscr_url = "tcp://localhost:5556"
-    filter_str = "TIB "
-    interval = 0.001
-    query = {'name': 'pathconf_check.py', 'argv': [subscr_url,filter_str]}
+    interval = 0.0
+    query = {'name': 'pathconf_check.py', 'argv': []}
     
     if len (sys.argv) == 2 and sys.argv[1] == 'uninstall':
         data = capi.uninstallQuery (tree, query)
--- examples/pathconf/pathconf_check.py
+++ examples/pathconf/pathconf_check.py
@@ -1,53 +1,20 @@
 import sys
-import zmq
 import json
 import pathdumpapi as pdi
 
-init = False
-socket = None
 FOUR_HOPS_LEN = 6 # [srcip,tor-agg,agg-core,core-agg,agg-tor,dstip]
 
-def initialize(argv):
-    global socket
 
-    url = str(argv[0])
-    filter_str = str(argv[1])
-    #  Socket to subscribe for flow records
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-
-    socket.connect(url)
-
-    # Python 2 - ascii bytes to unicode str
-    if isinstance(filter_str, bytes):
-       filter_str = filter_str.decode('ascii')
-    print("Collecting flow stats host agent",url,filter_str)
-    socket.setsockopt_string(zmq.SUBSCRIBE, filter_str)
-    
-
-def run(argv, collection):
-    global init,socket,FOUR_HOPS_LEN
-    if not init:
-        initialize(argv)
-        init = True
-    
-    if socket:
-        print "waiting for data"
-        string = socket.recv_string()
-        filter_str=str(argv[1])
-        string = string[len(filter_str):]
-        flow_record = json.loads(string)
-        path = flow_record['path']
-        if len(path) > FOUR_HOPS_LEN:
-            flowID = {}
-            reason = 'PC_FAIL'
-            data = {'path':path}
-            flowID['sip']   = flow_record['sip']
-            flowID['sport'] = flow_record['sport']
-            flowID['dip']   = flow_record['dip']
-            flowID['dport'] = flow_record['dport']
-            flowID['proto'] = flow_record['proto']
-            pdi.postFlow(flowID, reason, [data])
-            return "Posted PC_FAIL flow record"
-        else:
-            return "Path is valid"
+def run(flowRecord):
+    global FOUR_HOPS_LEN
+    path = flowRecord['path']
+    if len(path) > FOUR_HOPS_LEN:
+        flowID = {}
+        reason = 'PC_FAIL'
+        data = {'path':path}
+        flowID['sip']   = flowRecord['sip']
+        flowID['sport'] = flowRecord['sport']
+        flowID['dip']   = flowRecord['dip']
+        flowID['dport'] = flowRecord['dport']
+        flowID['proto'] = flowRecord['proto']
+        pdi.postFlow(flowID, reason, [data])
