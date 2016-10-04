edaee2103865972d73808a1ff9e163d8304606b0
Praveen Tammana <praveen.thammana@gmail.com>
2016-10-04 02:33:44 +0100

    changed patch file path

--- install.sh
+++ install.sh
@@ -20,11 +20,12 @@ cd cppzmq && sudo cp zmq.hpp zmq_addon.hpp /usr/local/include/
 cd ../
 sudo pip install pyyaml
 sudo pip install watchdog
+sudo pip install pyzmq
 # ovs checkout to older version, applies patch and then build
 git clone https://github.com/openvswitch/ovs.git
 cd ovs
 git checkout abcf3ef4c3f75963f46a11501685363e3ceb7c0e
-git apply ../../host/ovs_patch/1_Gig.diff
+git apply ../../host/ovs-patch/ovs-linux-kernel.diff
 ./boot.sh
 ./configure --with-linux=/lib/modules/`uname -r`/build
 make -j 8
