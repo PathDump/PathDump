56dae9fd25e1457fe1d71e8ac370b5f3af3591b0
Praveen Tammana <praveen.thammana@gmail.com>
2016-10-06 19:39:41 +0100

    updated ovs install

---
+++ host/ovs/start_vswitch.sh
@@ -0,0 +1,19 @@
+#Below commands kill/remove (if one running) openvswitch dependencies (datapathmodule, userspace modules) 
+#and restarts them in "/usr/local/var/run directory. So run this script with sudo permissions.
+kill `cd /usr/local/var/run/openvswitch && cat ovsdb-server.pid ovs-vswitchd.pid`
+make
+make modules_install
+rmmod openvswitch
+modprobe libcrc32c
+modprobe vxlan
+modprobe gre
+insmod $(pwd)/ovs/datapath/linux/openvswitch.ko
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+             --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+             --private-key=db:Open_vSwitch,SSL,private_key \
+             --certificate=db:Open_vSwitch,SSL,certificate \
+             --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+             --pidfile --detach
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
+
