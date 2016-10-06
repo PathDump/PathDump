3ee84d4ceefd262ec718e99f6a934c30d5ac8151
myungjin <myungjin.lee@ed.ac.uk>
2016-10-06 17:34:28 +0100

    update installation script

--- examples/topk/topk.py
+++ examples/topk/topk.py
@@ -10,10 +10,6 @@ def aggregate (topk, data):
     return h
 
 if __name__ == "__main__":
-    # tree = {'controller': {'parent': 'controller', 'child': ["172.17.0.3"]},
-    #         '172.17.0.3': {'parent': 'controller', 'child': ['172.17.0.4', '172.17.0.5']},
-    #         '172.17.0.4': {'parent': '172.17.0.3', 'child': []},
-    #         '172.17.0.5': {'parent': '172.17.0.3', 'child': []}}
     topk = 10
     linkID = ('*', '*')
     timeRange = ('*', '*')
--- install.sh
+++ install.sh
@@ -1,32 +1,40 @@
+mkdir tmpbuild
+pushd tmpbuild
+
 # Install steps for mongo-cxx (note: mongo-c and libbson dependency is there)
 wget https://github.com/mongodb/mongo-cxx-driver/archive/legacy-1.1.2.zip
 unzip legacy-1.1.2.zip 
-cd mongo-cxx-driver-legacy-1.1.2
+pushd mongo-cxx-driver-legacy-1.1.2
 sudo scons --prefix /usr/local --c++11=on install
-cd ../
+popd
+
 # Install steps for libzmq
 git clone https://github.com/zeromq/libzmq
-cd libzmq
+pushd libzmq
 ./autogen.sh
 ./configure     # add other options here
 make
 make check
 sudo make install
 sudo ldconfig
-cd ../
+popd
+
 #zmq cpp binding installation
 git clone https://github.com/zeromq/cppzmq
-cd cppzmq && sudo cp zmq.hpp zmq_addon.hpp /usr/local/include/
-cd ../
+pushd cppzmq && sudo cp zmq.hpp zmq_addon.hpp /usr/local/include/
+popd
 sudo pip install pyyaml
 sudo pip install watchdog
 sudo pip install pyzmq
+
 # ovs checkout to older version, applies patch and then build
 git clone https://github.com/openvswitch/ovs.git
-cd ovs
+pushd ovs
 git checkout abcf3ef4c3f75963f46a11501685363e3ceb7c0e
-git apply ../../host/ovs-patch/ovs-linux-kernel.diff
+git apply ../../host/ovs-patch/ovs-linux-kernel.patch
 ./boot.sh
 ./configure --with-linux=/lib/modules/`uname -r`/build
 make -j 8
+popd
 
+popd
