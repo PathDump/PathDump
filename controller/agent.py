f1bcb803a18183830833cf2d7180de14aac1327c
Myungjin Lee <myungjin.lee@ed.ac.uk>
2016-10-17 16:22:46 +0100

    update controller agent

--- controller/agent.py
+++ controller/agent.py
@@ -1,4 +1,5 @@
-#!flask/bin/python
+#!/usr/bin/python
+
 from flask import Flask
 from flask import make_response
 from flask import abort
@@ -8,8 +9,11 @@ import json
 import processquery as pq 
 import sys
 import confparser as cp
-import os
 import aggtree
+import os
+import shutil
+import hashlib
+
 
 app = Flask(__name__)
 
@@ -37,17 +41,32 @@ def getpathdumpget():
         content = pq.handlerequest (request.json, "pathdump")
         return content
 
+def md5 (fname):
+    hash_md5 = hashlib.md5()
+    with open (fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update (chunk)
+    return hash_md5.hexdigest()
+
 def initialize ():
     if len (sys.argv) == 2:
         cp.parse_config (sys.argv[1])
 
     # create app repository if it doesn't exist
-    if not os.path.exists (cp.options['home']+'/'+cp.options['repository']):
-        os.makedirs (cp.options['home']+'/'+cp.options['repository'])
+    directory = cp.options['home'] + '/' + cp.options['repository']
+    if not os.path.exists (directory):
+        os.makedirs (directory)
+        shutil.copy ('retrieve_flow.py', directory + '/retrieve_flow.py')
+        md5val = md5('retrieve_flow.py')
+        md5file = directory + '/retrieve_flow.py.md5'
+        with open (md5file, "w") as f:
+            f.write (md5val + '\n')
+        f.close()
 
     # create flowrecord collection directory if it doesn't exist
-    if not os.path.exists (cp.options['home']+'/'+cp.options['collection']):
-        os.makedirs (cp.options['home']+'/'+cp.options['collection'])
+    directory = cp.options['home']+'/'+cp.options['collection']
+    if not os.path.exists (directory):
+        os.makedirs (directory)
 
     aggtree.buildGroupTree (cp.options['home']+'/'+cp.options['grouptree'])
 
@@ -55,5 +74,4 @@ if __name__ == '__main__':
     initialize ()
     app.run (debug = True, host = "0.0.0.0")
 
-    # TODO: Run tests in a host OS
     # app.run (debug = True, host = "0.0.0.0", processes = 2)
