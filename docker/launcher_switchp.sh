360825245e94df16fdb78f81119bd8e3b1a4962b
praveen <praveen.thammana@gmail.com>
2017-08-11 15:16:11 +0100

    modified launch script

--- docker/launcher_switchp.sh
+++ docker/launcher_switchp.sh
@@ -1,17 +1,20 @@
-docker stop pod1.nsl003.inst0
-docker rm pod1.nsl003.inst0
-docker stop pod1.nsl003.inst1
-docker rm pod1.nsl003.inst1
-docker stop pod1.nsl003.inst2
-docker rm pod1.nsl003.inst2
-docker stop pod1.nsl003.inst3
-docker rm pod1.nsl003.inst3
+#!/bin/bash
+
+hostname=$1
+docker stop pod1.$hostname.inst0
+docker rm pod1.$hostname.inst0
+docker stop pod1.$hostname.inst1
+docker rm pod1.$hostname.inst1
+docker stop pod1.$hostname.inst2
+docker rm pod1.$hostname.inst2
+docker stop pod1.$hostname.inst3
+docker rm pod1.$hostname.inst3
 weave stop
 weave stop-proxy
 eval $(weave env --restore)
 weave launch nsl002
 eval $(weave env)
-docker run --name pod1.nsl003.inst0 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh
-docker run --name pod1.nsl003.inst1 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh 
-docker run --name pod1.nsl003.inst2 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh 
-docker run --name pod1.nsl003.inst3 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh
+docker run --name pod1.$hostname.inst0 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh
+docker run --name pod1.$hostname.inst1 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh 
+docker run --name pod1.$hostname.inst2 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh 
+docker run --name pod1.$hostname.inst3 -P -d -v /home/praveen/switchP/tool:/code -it praveen667/switchp:switchP_host /bin/bash /code/docker/start.sh
